18087e940adea4388db8cf6958b0dc8ca4ec3374
khourysmith <khoury.smith@gmail.com>
2020-07-28 22:52:04 -0400

    stroller show page almost complete

--- src/components/Stroller/Header.js
+++ src/components/Stroller/Header.js
@@ -1,22 +1,53 @@
 import React from 'react';
-import 'react-bulma-components/dist/react-bulma-components.min.css';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+    padding: 50px 100px 0;
+
+    img{
+        height:350px;
+        width:350px
+        margin-bottom:10px;
+        
+    }
+    h1{
+        font-size: 30px
+    
+    }
+
+`
+const AllReviews = styled.div`
+    font-size: 18px;
+    padding: 10px 0;
+`
+const Total = styled.div`
+    font-size:18px;
+    font-weight: bold;
+    padding: 10px 0
+`
 
 const Header = (props) => {
     const {name, image_url, average_score} = props.attributes
     const total = props.reviews.length
     return (
-        <div className="wrapper">
-            <img src={image_url}/>
-            <h1 title is-3>{name}</h1>
-            <div>
-                <div className="totalReviews">
-                    <p is-5>{total} User Reviews</p>
-                    </div>
-                <div className="rating"></div>
-    <div className="totalOf3">{average_score} out of 5</div>
+        <>
+        <div class="columns ">
+            <div className="column ">
+            <h1 className="is-size-3 mt-4"><img  src={image_url} alt={name}/> {name}</h1>
             </div>
-            
         </div>
+            
+                <div class="columns">
+                    <div className="column">
+                    <p>{total} User Reviews</p>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                    <Total>{average_score} out of 5</Total>
+                    </div>   
+        </div>
+        </>
     );
 };
 
--- src/components/Stroller/Stroller.js
+++ src/components/Stroller/Stroller.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import  { useState, useEffect } from 'react';
+import  { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import styled from "styled-components"
 import Form from './Form'
 import Navbar from '../Navbar';
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+    background-color: #ffffff;
+    height: 100vh;
+    overflow: scroll:
+`
+const Formcolumn = styled.div`
+    background-color: #ffc114;
+`
+const Main = styled.div`
+    padding-left: 50px
+`
+    
 
 
 
@@ -31,32 +50,67 @@ const Stroller = (props) => {
         })
       }, [])
 
+      const handleChange = (event) => {
+        event.preventDefault()
+
+        setReview(Object.assign({}, review,{[event.target.name]: event.target.value}))
+        // console.log("name:", event.target.name, "value:", event.target.value)
+        console.log("review:", review)
+
+      }
+
+      const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const token = document.querySelector('[name="csrf-token"]') || {content: 'no-csrf-token'}
+        const ax = axios.create({
+          headers: {
+            common: {
+              'X-CSRF-Token': token.content
+            }
+          }
+        })
+
+        const stroller_id = stroller.data.id
+        axios.post('http://localhost:3000/api/v1/reviews', {review, stroller_id})
+        .then(resp => {
+            const included = [ ...stroller.included, resp.data ]
+            setStroller({ ...stroller, included })
+            setReview({title: '', description: '', score: 0})
+        })
+        .catch(resp => {})
+    }
+
     return (
-        <>
-        <Navbar/>
-        <div className= "columns  is-vcentered ">
-           
-            
-            <div className="column is-four-fifths">
-                { 
+  <Wrapper>
+      { 
+            loaded &&
+            <Fragment> 
+                <Column>
+                    <Main>
+                    
+                        <Header
+                            attributes ={stroller.data.attributes}
+                            reviews = {stroller.included}
+                        />
                 
-                 loaded &&
-                <Header 
-                    attributes={stroller.data.attributes}
-                    reviews={stroller.included}
-                />
-                }
-                </div>
-                <div className="columns"></div>
-            
-           
-            <div className ="is-warning">
-                <Form/>
-            </div>
-          
-           
-        </div>
-        </>
+                    <div className="reviews"></div>
+                    </Main>
+                </Column>
+                <Formcolumn>
+                    <Form
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                    attributes ={stroller.data.attributes}
+                    review = {review}
+                    />
+                </Formcolumn>
+            </Fragment>
+     }
+
+  </Wrapper>
+
+        
     );
 };
 
